7e64ce1f7eaaa2b8f0d2f25719217f732038cd39
Aramis Araujo <aramis.araujo@ccc.ufcg.edu.br>
2018-11-09 00:37:25 -0300

    Implementa forma inicial de visualização

--- src/br/edu/ufcg/atg/graphs/App.java
+++ src/br/edu/ufcg/atg/graphs/App.java
@@ -1,7 +1,9 @@
 package br.edu.ufcg.atg.graphs;
 
 
+import org.jgraph.JGraph;
 import org.jgrapht.*;
+import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.*;
 import org.jgrapht.traverse.*;
 import java.net.*;
@@ -23,6 +25,17 @@ public class App {
     	
     	System.out.println(graph.vertexSet().size());
     	System.out.println(graph.edgeSet().size());
+    	
+    	
+    	Viewer viewer = new Viewer();
+    	
+    	viewer.init(graph);
+    	
+//        // create a JGraphT graph
+//    	ListenableGraph<String, DefaultWeightedEdge> g = new ListenableGraph<String,DefaultWeightedEdge>( DefaultEdge.class );
+//
+//        // create a visualization using JGraph, via the adapter
+//         JGraph jgraph = new JGraph( new JGraphModelAdapter( graph ) );
     }
     
     private static List<String> readNodes() {
